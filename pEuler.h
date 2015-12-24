8846d737b1b90d966b07b31a52ada58e11734622
taumataa <taumata@hotmail.co.uk>
2015-12-24 15:33:51 +1300

    Create pEuler.h

---
+++ pEuler.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ANUM 1
+#define MAXNUM1 600
+#define MAXVAL2 4000000
+#define QVAL3 600851475143
+
+bool IsPrime(unsigned long);
+
+double * questionOne();
+double * questionTwo();
+double * questionThree();
+double * questionFour();
+double * questionFive();
+double * questionSix();
+double * questionSeven();
+double * questionEight();
